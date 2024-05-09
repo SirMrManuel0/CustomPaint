f5c6c928a1dc25c2ab96d4db99f22f2d91fe722f
SirMrManuel0 <139504269+SirMrManuel0@users.noreply.github.com>
2024-05-09 18:00:07 +0200

    add LICENSE.txt

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import gui.PaintMain;
 
 public class Main {
     public static void main(String[] args) {
+        // last check commit "add move drawn element" 09.05.2024: 2,826 lines of code
         new PaintMain();
     }
 }
